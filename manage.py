a6f70290dcf612d0cfe8afcc5aaf8105d488942b
marsanem <octet.software@gmail.com>
2014-10-08 13:05:11 -0500

    Resumen actividades hasta la pag. 38 de Flask

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 from flask.ext.script import Manager
 from sched.app import app
+
 manager = Manager(app)
-app.config['DEBUG'] = True # Ensure debugger will load.
-if __name__ == '__main__':
-   manager.run()
+app.config['DEBUG'] = True
+
+if __name__ == "__main__":
+    manager.run()
--- sched/app.py
+++ sched/app.py
@@ -1,12 +1,10 @@
-
-from flask import Flask, make_response
-from flask import url_for
 from flask import Flask
+from flask import url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 from sched.models import Base
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sched.db'
 # Use Flask-SQLAlchemy for its engine and session
 # configuration. Load the extension, giving it the app object,
@@ -15,55 +13,49 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sched.db'
 db = SQLAlchemy(app)
 db.Model = Base
 
-@app.route('/')
-
-@app.route('/appointments/')
-def appointment_list():
-   return 'Listing of all appointments we have.'
-
-@app.route('/appointments/<int:appointment_id>/')
-def appointment_detail(appointment_id):
-   edit_url = url_for('appointment_edit',
-      appointment_id=appointment_id)
-   # Return the URL string just for demonstration.
-   return edit_url
-
-@app.route(
-   '/appointments/<int:appointment_id>/edit/',
-   methods=['GET', 'POST'])
-
-
-def appointment_edit(appointment_id):
-   return 'Form to edit appointment #.'.format(appointment_id)
-
-@app.route(
-   '/appointments/create/',
-   methods=['GET', 'POST'])
-
-def appointment_create():
-   return 'Form to create a new appointment.'
-
-@app.route(
-   '/appointments/<int:appointment_id>/delete/',
-   methods=['DELETE'])
-
-def appointment_delete(appointment_id):
-   raise NotImplementedError('DELETE')
-
-app = Flask(__name__)
 @app.route('/string/')
 def return_string():
-   return 'Hello, world!'
+        return 'Hello, world!'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/object/')
 def return_object():
-   headers = {'Content-Type': 'text/plain'}
-   return make_response('Hello, world!', status=200,
-      headers=headers)
+    headers = {'Content-Type': 'text/plain'}
+    return make_response('Hello, world!', status=200,headers=headers)
 
 @app.route('/tuple/')
 def return_tuple():
-   return 'Hello, world!', 200, {'Content-Type':
-      'text/plain'}
+    return 'Hello, world!', 200, {'Content-Type':'text/plain'}
 
+@app.route('/appointments/')
+def appointment_list():
+    return 'Listing of all appointments we have.'
 
+@app.route('/appointments/<int:appointment_id>/',endpoint='some_name')
+def appointment_detail(appointment_id):
+    edit_url = url_for('appointment_edit',appointment_id=appointment_id)
+    # Return the URL string just for demonstration.
+    return edit_url
+
+@app.route(
+    '/appointments/<int:appointment_id>/edit/',
+    methods=['GET', 'POST'])
+
+def appointment_edit(appointment_id):
+    return 'Form to edit appointment #.'.format(appointment_id)
+
+@app.route(
+    '/appointments/create/', methods=['GET', 'POST'])
+def appointment_create():
+    return 'Form to create a new appointment.'
+
+@app.route(
+    '/appointments/<int:appointment_id>/delete/',methods=['DELETE'])
+def appointment_delete(appointment_id):
+    raise NotImplementedError('DELETE')
+
+if __name__ == "__main__":
+    app.run()
